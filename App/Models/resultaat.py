2816d61cdf7bd0df45e75a280306921d19a38dcb
Deflation8449 <169663220+Deflation8449@users.noreply.github.com>
2024-05-29 10:10:56 -0400

    fix: implemented id in resultaat base

--- App/Models/resultaat.py
+++ App/Models/resultaat.py
@@ -1,6 +1,7 @@
-from pydantic import BaseModel, PastDate
+from pydantic import BaseModel, PastDate, UUID5
 
 class ResultaatBase(BaseModel):
+    id: UUID5
     onderzoek: None  # dit vervangen met onderzoek class
     naam: str
     datum: PastDate
