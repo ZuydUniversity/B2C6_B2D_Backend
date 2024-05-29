60267692d9ec03e8ce55c9579af5a3b2a263f9e4
Deflation8449 <169663220+Deflation8449@users.noreply.github.com>
2024-05-29 10:09:17 -0400

    feat: added resultaat class

--- App/__init__.py
+++ App/__init__.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 
+from App.Routes.resultaat import router as resultaat_router
+
 def initializeApp():
     app = FastAPI()
+    app.include_router(resultaat_router)
+
     return app
