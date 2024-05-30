6b2d47132aa33f86310e839e13aebaf3f8e76eea
SaidAMG <2104440amghar@zuyd.nl>
2024-05-30 23:44:20 +0200

    init

--- App/__init__.py
+++ App/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+from .Routes import ZorgverlenerRoute
 
 def initializeApp():
     app = FastAPI()
+    app.include_router(ZorgverlenerRoute.router)
     return app
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from App import initializeApp
 
 app = initializeApp()
+
+@app.get("/")
+async def root():
+    return {"data": "Hello Wolrd"}
