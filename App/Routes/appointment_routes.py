2a9b7aea0d41a7c5bbb92fc05416f0d9725e1caa
Bob Keulen <2103861keulen@zuyd.nl>
2024-05-30 20:57:06 +0200

    Added Appointment read features + endpoints

--- App/Routes/appointment_routes.py
+++ App/Routes/appointment_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from typing import List, Optional
 from ..Models.appointment_model import Appointment
 
@@ -11,3 +11,12 @@ def create_appointment(appointment: Appointment):
     sim_data.append(appointment)
     return appointment
 
+@router.get("/appointments/", response_model=List[Appointment])
+def read_appointments():
+    return sim_data
+
+@router.get("/appointments/{appointment_id}", response_model=Appointment)
+def read_appointment(appointment_id: int):
+    for appointment in sim_data:
+        if appointment.id == appointment_id:
+            return appointment
