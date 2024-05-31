331a0f206e2c3f8ae5461c6f1520c32ace8ef197
ZordTra <Caspervdp2a@gmail.com>
2024-05-31 14:15:34 +0200

    create added

--- App/Routes/resultaat.py
+++ App/Routes/resultaat.py
@@ -10,3 +10,7 @@ router = APIRouter()
 @router.get("/resultaat/{id}", response_model=ResultaatBase)
 async def get_resultaat(user_id: UUID):
     return resultaat_get_by_id(user_id)
+
+@router.post("/items?{item_id}")
+async def Create_Resultaat(resultaat: ResultaatBase):
+    return {"resultaat_name": resultaat.naam,"Resultaat_date": resultaat.datum,"resultaat_discription": resultaat.omschrijving}
