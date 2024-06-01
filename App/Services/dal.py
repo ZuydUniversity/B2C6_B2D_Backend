c1c903fa47b9f8af5a9cb91c700051e3f11fa46c
Jeroen Stekelenburg <1747142stekelenburg@zuyd.nl>
2024-06-01 12:54:30 +0200

    fix voor missende variable

--- App/Services/dal.py
+++ App/Services/dal.py
@@ -1,7 +1,7 @@
 from uuid import UUID
 
 from App.Models.resultaat import ResultaatBase
-from App.Services.db import resultaat_get_data_by_id
+from App.Services.db import resultaat_get_data_by_id, resultaat_update_data_by_id
 
 def resultaat_get_by_id(user_id: UUID) -> ResultaatBase:
     RESULTAAT_DATA = resultaat_get_data_by_id(user_id)
