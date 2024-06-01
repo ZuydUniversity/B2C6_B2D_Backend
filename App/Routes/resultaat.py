134a0d71168d7f20f580402c464636a2da585333
Cunqex <benoit6705@gmail.com>
2024-06-01 13:42:43 +0200

    feature Delete added

--- App/Routes/resultaat.py
+++ App/Routes/resultaat.py
@@ -3,6 +3,7 @@ from uuid import UUID
 
 from App.Models.resultaat import ResultaatBase
 from App.Services.dal import resultaat_get_by_id
+from App.Services.dal import resultaat_delete_by_id
 
 
 router = APIRouter()
@@ -14,3 +15,9 @@ async def get_resultaat(user_id: UUID):
 @router.post("/items?{item_id}")
 async def Create_Resultaat(resultaat: ResultaatBase):
     return {"resultaat_name": resultaat.naam,"Resultaat_date": resultaat.datum,"resultaat_discription": resultaat.omschrijving}
+
+
+@router.delete("/resultaat/{id}", response_model=dict)
+async def delete_resultaat(id: UUID):
+    resultaat_delete_by_id(id)  
+    return {"detail": "Resultaat is met succes verwijderd"}
