d1cf99ae435e82afd0cb634b464b56dfc5bd2e86
PieDePopper <2200279ogg@zuyd.nl>
2024-06-05 11:50:18 +0200

    test

--- backend/main.py
+++ backend/main.py
@@ -55,12 +55,12 @@ if __name__ == "__main__":
     db = SessionLocal()
 
     # Create
-    new_report = create_report(db, date="2024-05-29", healthcomplaints="Headache, nausea", medicalhistory="None", diagnose="Migraine2")
-    print(f"Created Report: {new_report.id}, {new_report.date}, {new_report.healthcomplaints}, {new_report.medicalhistory}, {new_report.diagnose}")
+    # new_report = create_report(db, date="2024-05-29", healthcomplaints="Headache, nausea", medicalhistory="None", diagnose="Migraine2")
+    # print(f"Created Report: {new_report.id}, {new_report.date}, {new_report.healthcomplaints}, {new_report.medicalhistory}, {new_report.diagnose}")
 
     # # Read
-    # report = read_report(db, 1)
-    # print(f"Read Report: {report.id}, {report.date}, {report.healthcomplaints}, {report.medicalhistory}, {report.diagnose}")
+    report = read_report(db, 4)
+    print(f"Read Report: {report.id}, {report.date}, {report.healthcomplaints}, {report.medicalhistory}, {report.diagnose}")
 
     # # Update
     # updated_report = update_report(db, 1, date="2024-06-01", healthcomplaints="Severe headache", diagnose="ok")
