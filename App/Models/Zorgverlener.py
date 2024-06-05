c0399a38470292a1e520b2d364c9cc0095d05e2e
Dennis Bückers <2105449buckers@zuyd.nl>
2024-06-05 10:04:54 +0200

    .

--- App/Models/Zorgverlener.py
+++ App/Models/Zorgverlener.py
@@ -1,25 +1,9 @@
+from pydantic import BaseModel
 
 
-class Zorgverlener:
-    
-    def CreateZorgverlener(self, _id: int, _name: str, _lastname: str, _email: str, _password: str):
-        self.id = _id
-        self.name = _name
-        self.lastname = _lastname
-        self.email = _email
-        self.password = _password
-
-    def get_id(self):
-        return self.id
-
-    def get_name(self):
-        return self.name
-
-    def get_lastname(self):
-        return self.lastname
-
-    def get_email(self):
-        return self.email
-
-    def get_password(self):
-        return self.password   
+class Zorgverlener(BaseModel):
+    id: int
+    naam: str
+    achternaam: str
+    email: str
+    wachtwoord: str
