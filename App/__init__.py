86fb87e47992991295b84bd6108666e22800416a
SaidAMG <2104440amghar@zuyd.nl>
2024-06-06 15:20:59 +0200

    .

--- App/__init__.py
+++ App/__init__.py
@@ -3,7 +3,7 @@ from .Routes import ZorgverlenerRoute
 from fastapi.middleware.cors import CORSMiddleware
 
 def initializeApp():
-    origins = ["52.166.135.125", "*", "127.0.0.1"]
+    origins = ["http://localhost", "http://52.166.135.125"]
     app = FastAPI()
     app.include_router(ZorgverlenerRoute.router)
     app.add_middleware(
