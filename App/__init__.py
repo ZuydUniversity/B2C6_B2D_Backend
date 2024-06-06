0ee6ef8cf625d6a4351364faa1964570da136b23
SaidAMG <2104440amghar@zuyd.nl>
2024-06-06 16:41:34 +0200

    ,

--- App/__init__.py
+++ App/__init__.py
@@ -5,11 +5,14 @@ from fastapi.middleware.cors import CORSMiddleware
 def initializeApp():
     origins = ["*"]
     app = FastAPI()
+
+    app.include_router(ZorgverlenerRoute.router)
+    
     app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],)
-    app.include_router(ZorgverlenerRoute.router)
+    
     return app
