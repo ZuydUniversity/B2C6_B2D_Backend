73de552c66b9304d92e494b1fdfad128f04a71d5
SaidAMG <2104440amghar@zuyd.nl>
2024-06-10 01:03:45 +0200

    databse models toegevoegd, ZorgevelenerRepo toegevoegd en Routes aan gepast

---
+++ App/Data/DatabaseModels.py
@@ -0,0 +1,13 @@
+from .Database import Base
+from sqlalchemy import Column, Integer, String
+
+class Zorgverlener(Base):
+    __tablename__ = "Zorgverleners"
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement="auto")
+    name = Column(String(50), nullable=False)
+    lastName = Column(String(50), nullable=False)
+    email = Column(String(50), unique=False, nullable=False)
+    phoneNumber = Column(Integer, nullable=False)
+    password = Column(String(50), nullable=False)
+    profession = Column(String(50), nullable=False)
