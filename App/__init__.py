fc8b1b6e4b55d3185ee9e11c25ccb2aca34d8523
Dennis Buckers <2105449buckers@zuyd.nl>
2024-06-10 00:55:09 +0200

    database connectie toegevoegd

--- App/__init__.py
+++ App/__init__.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
 from .Routes import ZorgverlenerRoute
 from fastapi.middleware.cors import CORSMiddleware
+from .Data import DataBaseModels 
+from .Data.Database import engine
+
 
 def initializeApp():
     origins = ["*"]
+
+    DataBaseModels.Base.metadata.create_all(bind=engine)
+
     app = FastAPI()
 
     app.include_router(ZorgverlenerRoute.router)
