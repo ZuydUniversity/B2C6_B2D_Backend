b2fc0f39cee70c50bbeff4c85269f8eb19970dc6
ZordTra <Caspervdp2a@gmail.com>
2024-06-11 00:21:43 +0200

    added the get function with database acces need to be tested

--- App/Models/resultaat.py
+++ App/Models/resultaat.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel, PastDate, UUID5
 
-class ResultaatBase(BaseModel):
-    id: UUID5
+class Resultaat(BaseModel):
+    #id: UUID5
+    id: int | None = None
     onderzoek: None  # dit vervangen met onderzoek class
-    naam: str
-    datum: PastDate
-    omschrijving: str
+    name: str
+    date: PastDate
+    discription: str
--- App/Routes/resultaat.py
+++ App/Routes/resultaat.py
@@ -1,7 +1,7 @@
 from App.Repos.ResultaatRepo import ResultaatRepo
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 #from uuid import UUID
-from App.Models.resultaat import ResultaatBase
+from App.Models.resultaat import Resultaat
 from App.Data.Database import get_db
 from sqlalchemy.orm import Session
 
@@ -10,19 +10,30 @@ from App.Services.dal import resultaat_delete_by_id
 
 
 router = APIRouter(prefix ="/resultaten")
+#get with id
+@router.get("/{id}")
+async def Get_Resultaat(id:int, db: Session = Depends(get_db)):
+    repo = ResultaatRepo
+    # check if exist
+    return await repo.Get_Resultaat(id)
 
-@router.get("/resultaat/{id}", response_model=ResultaatBase)
-async def get_resultaat(user_id: UUID):
-    return resultaat_get_by_id(user_id)
+#get all
+async def GetAll_Resultaat(db: Session = Depends(get_db)):
+    Resultaten = await ResultaatRepo(db).GetAll_Resultaat()
+    return Resultaten
+# @router.get("/resultaat/{id}", response_model=ResultaatBase)
+# async def get_resultaat(user_id: UUID):
+#     return resultaat_get_by_id(user_id)
 
+#post one
 @router.post("")
-async def add_Resultaat(resultaat: ResultaatBase, db:Session = Depends(get_db)):
+async def add_Resultaat(resultaat: Resultaat, db:Session = Depends(get_db)):
     repo = ResultaatRepo(db)
     newResultaat = await repo.add_resultaat(resultaat)
-    return {"resultaat_name": resultaat.naam,"Resultaat_date": resultaat.datum,"resultaat_discription": resultaat.omschrijving}
+    return {"resultaat_name": resultaat.name,"Resultaat_date": resultaat.date,"resultaat_discription": resultaat.discription}
 
-
-@router.delete("/resultaat/{id}", response_model=dict)
-async def delete_resultaat(id: UUID):
-    resultaat_delete_by_id(id)  
-    return {"detail": "Resultaat is met succes verwijderd"}
+#delete by id
+# @router.delete("/resultaat/{id}", response_model=dict)
+# async def delete_resultaat(id: UUID):
+#     resultaat_delete_by_id(id)  
+#     return {"detail": "Resultaat is met succes verwijderd"}
