221488acaf5c6b7c5614704891ce701ced8ef036
DidierV <2205137vervier@zuyd.nl>
2024-06-12 10:53:13 +0200

    connectionstring

--- App/Data/Database.py
+++ App/Data/Database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "<ConnectionString>"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root@localhost/db2"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
