29cacfcb6553bc2e24995a5782657d92d3255cb9
Hassan Mahmoud <2065740mahmoud@zuyd.nl>
2024-06-13 00:26:34 +0200

    initial push api groep 6

--- App/Data/Database.py
+++ App/Data/Database.py
@@ -10,7 +10,7 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
+ 
 # Dependency
 def get_db():
     db = SessionLocal()
