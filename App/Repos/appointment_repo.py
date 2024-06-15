cb3a6c3b5655e34ca85fdc74bdb630edcbc07872
Bob Keulen <2103861keulen@zuyd.nl>
2024-06-15 17:02:35 +0200

    Added decryption functionality to Appointment model

--- App/Repos/appointment_repo.py
+++ App/Repos/appointment_repo.py
@@ -7,6 +7,8 @@ class AppointmentRepo:
 
     def get_appointments(db: Session):
         appointments = db.query(dbmodels.Appointment).all()
+        for appointment in appointments:
+            appointment.decrypt()
         return appointments
     
     def get_appointment_by_id(id: int, db: Session):
@@ -14,7 +16,7 @@ class AppointmentRepo:
         if not appointment:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                                 detail=f"Appointment with id: {id} does not exist")
-
+        appointment.decrypt()
         return appointment
     
     def create_appointment(appointment: Appointment, db: Session):
@@ -34,7 +36,18 @@ class AppointmentRepo:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                                 detail=f"Appointment with id: {id} does not exist")
 
-        query.update(appointment.model_dump(), synchronize_session=False)
+        existing_appointment.decrypt()
+
+        updated_values = appointment.model_dump()
+        for key, value in updated_values.items():
+            setattr(existing_appointment, key, value)
+
+        # Encrypt de bijgewerkte afspraak
+        existing_appointment.encrypt()
+
+        # Gebruik de update method van SQLAlchemy om de gegevens bij te werken
+        db.query(dbmodels.Appointment).filter(dbmodels.Appointment.id == id).update(
+        updated_values, synchronize_session=False)
         db.commit()
         return query.first()
 
