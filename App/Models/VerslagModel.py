990a60cec1f14e1b7885f0d7a11ba6067f0e8e95
PieDePopper <2200279ogg@zuyd.nl>
2024-06-19 20:24:27 +0200

    Crud functie toegevoegd (werkt nu)

--- App/Models/VerslagModel.py
+++ App/Models/VerslagModel.py
@@ -1,8 +1,11 @@
-from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String
+from App.Data.Database import Base  # Zorg ervoor dat je Base van de juiste plaats importeert
 
-class Verslag(BaseModel):
-    id : int | None = None 
-    date: str
-    healthcomplaints: str
-    medicalhistory: str
-    diagnose: str
+class Verslag(Base):
+    __tablename__ = "verslagen1"
+
+    id = Column(Integer, primary_key=True, index=True)
+    date = Column(String(100), index=True)
+    healthcomplaints = Column(String(500), index=True)
+    medicalhistory = Column(String(500), index=True)
+    diagnose = Column(String(100), index=True)
