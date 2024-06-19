41e14820c91b42e86a0edb11e2265681fb29466f
PieDePopper <2200279ogg@zuyd.nl>
2024-06-19 11:39:37 +0200

    Crud functies toegevoegd in VerslagRepo

---
+++ App/Repos/VerslagRepo.py
@@ -0,0 +1,55 @@
+from sqlalchemy.orm import Session
+from ..Models.VerslagModel import Verslag 
+from App.Data import DatabaseModels as dbmodels
+
+class ZorgverlenersRepo:
+    def __init__(self, db: Session):
+        self.db = db
+
+
+    async def add_verslag(db_session: Session, date, healthcomplaints, medicalhistory, diagnose): #run python -m backend.main
+        verslag = Verslag(
+            date=date,
+            healthcomplaints=healthcomplaints,
+            medicalhistory=medicalhistory,
+            diagnose=diagnose
+        )
+        db_session.add(verslag)
+        db_session.commit()
+        db_session.refresh(verslag)
+        return verslag
+
+    async def get_verslag(db_session: Session, verslag_id):
+        return db_session.query(Verslag).filter(Verslag.id == verslag_id).first()
+    
+    async def get_verslagen(db_session: Session):
+        return db_session.query(Verslag).all()
+
+    async def update_verslag(db_session: Session, verslag_id, date=None, healthcomplaints=None, medicalhistory=None, diagnose=None):
+        verslag = db_session.query(Verslag).filter(Verslag.id == verslag_id).first()
+        if verslag:
+            if date:
+                verslag.date = date
+            if healthcomplaints:
+                verslag.healthcomplaints = healthcomplaints
+            if medicalhistory:
+                verslag.medicalhistory = medicalhistory
+            if diagnose:
+                verslag.diagnose = diagnose
+            db_session.commit()
+            db_session.refresh(verslag)
+        return verslag
+    
+    async def delete_verslag(db_session: Session, verslag_id):
+        verslag = db_session.query(Verslag).filter(Verslag.id == verslag_id).first()
+        if verslag:
+            db_session.delete(verslag)
+            db_session.commit()
+        return verslag
+    
+
+    
+
+
+
+
