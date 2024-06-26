641145b552e0956293f37c6b0b5a38a58071bdf6
SaidAMG <2104440amghar@zuyd.nl>
2024-06-26 02:59:25 +0200

    build(deps): update DatabaseModels.py, ZorgverlenerModel.py and requirements.txt

--- App/Data/DatabaseModels.py
+++ App/Data/DatabaseModels.py
@@ -1,8 +1,8 @@
 from .Database import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 
 class Zorgverlener(Base):
-    __tablename__ = "Zorgverleners"
+    __tablename__ = "zorgverleners"
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement="auto")
     name = Column(String(50), nullable=False)
@@ -11,3 +11,6 @@ class Zorgverlener(Base):
     phoneNumber = Column(String(15), nullable=False)
     password = Column(String(50), nullable=False)
     profession = Column(String(50), nullable=False)
+    isActive = Column(Boolean(True), nullable=False)
+    
+    
