d02519a896e64e9ec537f912608ac92a07027101
Deflation8449 <169663220+Deflation8449@users.noreply.github.com>
2024-06-27 09:01:34 -0400

    fix: env vars handling

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+# niet bedoelt om met docker te runnen
+
+# load env vars before running webserver
+from dotenv import load_dotenv
+import os
+
+load_dotenv(".env.local")
+
+print(f"[*] using db: {os.getenv('DB_URL')}")
+
+# start webserver
 from App import initializeApp
 
 app = initializeApp()
+
+
