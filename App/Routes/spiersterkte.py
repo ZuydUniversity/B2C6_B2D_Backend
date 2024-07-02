e7bfe575891887e3bcd7d1df1b7bc9dffb41c799
Deflation8449 <169663220+Deflation8449@users.noreply.github.com>
2024-07-02 14:58:31 -0400

    feat: implemented routes for spiersterkte

--- App/Routes/spiersterkte.py
+++ App/Routes/spiersterkte.py
@@ -1,7 +1,7 @@
-from App.Repos.ResultaatRepo import ResultaatRepo
+from App.Repos.SpiersterkteRepo import SpiersterkteRepo
 from fastapi import APIRouter, Depends, HTTPException, status
 #from uuid import UUID
-from App.Models.resultaat import ResultaatIn, ResultaatDb
+from App.Models.spiersterkte import SpiersterkteIn, SpiersterkteDb
 from App.Data.Database import get_db
 from sqlalchemy.orm import Session
 
@@ -9,29 +9,29 @@ router = APIRouter(prefix ="/resultaten")
 
 
 #get all
-@router.get("/", response_model=list[ResultaatDb])
-async def GetAll_Resultaat(db: Session = Depends(get_db)):
-    return await ResultaatRepo(db).GetAll_Resultaat()
+@router.get("/", response_model=list[SpiersterkteDb])
+async def GetAll_Spiersterkte(db: Session = Depends(get_db)):
+    return await SpiersterkteRepo(db).GetAll_Spiersterkte()
 
 #get with id
-@router.get("/{id}", response_model=ResultaatDb)
-async def Get_Resultaat(id:int, db: Session = Depends(get_db)):
-    return await ResultaatRepo(db).Get_Resultaat(id)
+@router.get("/{id}", response_model=SpiersterkteDb)
+async def Get_Spiersterkte(id:int, db: Session = Depends(get_db)):
+    return await SpiersterkteRepo(db).Get_Spiersterkte(id)
 
 #post one
-@router.post("/", response_model=ResultaatDb)
-async def add_Resultaat(resultaat: ResultaatIn, db: Session = Depends(get_db)):
-    return await ResultaatRepo(db).add_Resultaat(resultaat)
+@router.post("/", response_model=SpiersterkteDb)
+async def add_Spiersterkte(spiersterkte: SpiersterkteIn, db: Session = Depends(get_db)):
+    return await SpiersterkteRepo(db).add_Spiersterkte(spiersterkte)
 
-@router.put("/{id}", response_model=ResultaatDb)
-async def update_Resultaat(id: int, resultaat: ResultaatIn, db: Session = Depends(get_db)):
-    updated_resultaat = await ResultaatRepo(db).update_Resultaat(id, resultaat)
-    if not updated_resultaat:
-        raise HTTPException(status_code=404, detail="Resultaat not found")
-    return updated_resultaat
+@router.put("/{id}", response_model=SpiersterkteDb)
+async def update_Spiersterkte(id: int, spiersterkte: SpiersterkteIn, db: Session = Depends(get_db)):
+    updated_spiersterkte = await SpiersterkteRepo(db).update_Spiersterkte(id, spiersterkte)
+    if not updated_spiersterkte:
+        raise HTTPException(status_code=404, detail="Spiersterkte not found")
+    return updated_spiersterkte
 
 
 @router.delete("/{id}", response_model=dict)
-async def delete_Resultaat(id: int, db: Session = Depends(get_db)):
-    await ResultaatRepo(db).delete_Resultaat(id)
-    return {"detail": "Resultaat is met succes verwijderd"}
+async def delete_Spiersterkte(id: int, db: Session = Depends(get_db)):
+    await SpiersterkteRepo(db).delete_Spiersterkte(id)
+    return {"detail": "Spiersterkte is met succes verwijderd"}
