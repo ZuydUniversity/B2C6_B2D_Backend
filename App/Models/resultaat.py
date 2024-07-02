224eab4323fb7025398a56305ea81086a7b96bac
Deflation8449 <169663220+Deflation8449@users.noreply.github.com>
2024-07-02 15:27:23 -0400

    fix: id in out model of resultaat

--- App/Models/resultaat.py
+++ App/Models/resultaat.py
@@ -1,14 +1,18 @@
 from pydantic import BaseModel
 from .spiersterkte import SpiersterkteOut
 
-class ResultaatIn(BaseModel):
+class ResultaatBase(BaseModel):
     # onderzoek: None  # dit vervangen met onderzoek class
     name: str
     date: str
     discription: str
+    
+class ResultaatIn(ResultaatBase):
+    ...
 
-class ResultaatDb(ResultaatIn):
+class ResultaatDb(ResultaatBase):
     id: int
     
-class ResultaatOut(ResultaatIn):
+class ResultaatOut(ResultaatBase):
+    id: int
     spiersterkten: list[SpiersterkteOut]
