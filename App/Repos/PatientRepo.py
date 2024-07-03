5e9860e926ca8df82629d339f2032a9a117ff2fc
Hassan Mahmoud <2065740mahmoud@zuyd.nl>
2024-07-03 13:08:31 +0200

    aangepast

--- App/Repos/PatientRepo.py
+++ App/Repos/PatientRepo.py
@@ -1,3 +1,43 @@
+# from sqlalchemy.orm import Session
+# from ..Models.PatientModel import Patient 
+# from App.Data import DatabaseModels as dbmodels
+
+# class PatientRepo:
+#     def __init__(self, db: Session):
+#         self.db = db
+
+#     async def patientExists(self, id:int):
+#         return self.db.query(dbmodels.Patient).filter(dbmodels.Patient.id == id).count()
+
+#     async def get_patients(self):
+#         Patients = self.db.query(dbmodels.Patient).all()
+#         return Patients
+
+#     async def get_patient(self, id: int):
+#         Patient = self.db.query(dbmodels.Patient).filter(dbmodels.Patient.id == id).first()
+#         return Patient
+
+#     async def add_patient(self, patient: Patient):
+#         patient.id = None
+#         new_patient = dbmodels.Patient(**patient.model_dump())
+#         self.db.add(new_patient)
+#         self.db.commit()
+#         self.db.refresh(new_patient)
+#         return new_patient
+    
+#     async def update_patient(self, id: int, patient: Patient):
+#         patient.id = id
+#         isUpdated = self.db.query(dbmodels.Patient).filter(dbmodels.Patient.id == id).update(patient.model_dump(), synchronize_session=False)
+#         self.db.commit()
+#         return isUpdated
+
+#     async def delete_patient(self, id: int):
+#         Deleted = self.db.query(dbmodels.Patient).filter(dbmodels.Patient.id == id).delete()
+#         self.db.commit()
+#         return Deleted
+
+# patientrepo.py
+
 from sqlalchemy.orm import Session
 from ..Models.PatientModel import Patient 
 from App.Data import DatabaseModels as dbmodels
@@ -6,32 +46,30 @@ class PatientRepo:
     def __init__(self, db: Session):
         self.db = db
 
-    async def patientExists(self, id:int):
+    async def patientExists(self, id: int):
         return self.db.query(dbmodels.Patient).filter(dbmodels.Patient.id == id).count()
 
     async def get_patients(self):
-        Patients = self.db.query(dbmodels.Patient).all()
-        return Patients
+        return self.db.query(dbmodels.Patient).all()
 
     async def get_patient(self, id: int):
-        Patient = self.db.query(dbmodels.Patient).filter(dbmodels.Patient.id == id).first()
-        return Patient
+        return self.db.query(dbmodels.Patient).filter(dbmodels.Patient.id == id).first()
 
     async def add_patient(self, patient: Patient):
         patient.id = None
-        new_patient = dbmodels.Patient(**patient.model_dump())
+        new_patient = dbmodels.Patient(**patient.dict())
         self.db.add(new_patient)
         self.db.commit()
         self.db.refresh(new_patient)
         return new_patient
-    
+
     async def update_patient(self, id: int, patient: Patient):
-        patient.id = id
-        isUpdated = self.db.query(dbmodels.Patient).filter(dbmodels.Patient.id == id).update(patient.model_dump(), synchronize_session=False)
+        patient_data = patient.dict(exclude_unset=True)
+        self.db.query(dbmodels.Patient).filter(dbmodels.Patient.id == id).update(patient_data)
         self.db.commit()
-        return isUpdated
+        return await self.get_patient(id)
 
     async def delete_patient(self, id: int):
-        Deleted = self.db.query(dbmodels.Patient).filter(dbmodels.Patient.id == id).delete()
+        deleted_count = self.db.query(dbmodels.Patient).filter(dbmodels.Patient.id == id).delete()
         self.db.commit()
-        return Deleted
+        return deleted_count > 0
